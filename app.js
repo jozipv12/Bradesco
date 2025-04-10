96a4c30cdb1974f9f82325ad32e7493b9d880c94
jozimara <jozipv12@gmail.com>
2025-04-10 18:44:26 -0300

    continua

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict'
+cont switcher=document.querySelector('.btn');
+switcher.addeventlist('click',function(){document.body.classList.toggle('dark-theme');
+var classnane-document.body.className;
+if classnane--("light-theme"){
+    this.textcontent-("dark")
+};
+else{
+    this.textcontent="light";}
+});
+...{
+console.log('current class nome:'+ classnane);
+});
